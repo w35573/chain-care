13f062c50c087569d80909725c77301b45170753
w35573 <prathamesh.redij@spit.ac.in>
2023-04-03 20:07:25 +0530

    first commit

---
+++ blockchain/truffle-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 30000000,
+      from: '0xe8543ceDAd017D57cda9EEA51322af431B84D46e',
+    },
+  },
+  contracts_directory: './testing/',
+  contracts_build_directory: './src/build/',
+  compilers: {
+    solc: {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 100
+        }
+      }
+    }
+  }
+}
---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import app from './app';
+import { initContract } from './config/contracts.config';
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, async () => {
+    await initContract();
+    console.log(`Server up on port ${PORT}!`);
+});
