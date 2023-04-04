5a85dcaf15dbd1a06742debf659138431d575f56
w35573 <prathamesh.redij@spit.ac.in>
2023-04-04 16:39:34 +0530

    resolved firebase bugs

--- blockchain/src/main_dashboard/views/Dashboard/Dashboard.js
+++ blockchain/src/main_dashboard/views/Dashboard/Dashboard.js
@@ -207,8 +207,8 @@ export default function Dashboard() {
                 tabIcon: BugReport,
                 tabContent: (
                   <Tasks
-                    checkedIndexes={[ 0, 3 ]}
-                    tasksIndexes={[ 0, 1, 2, 3 ]}
+                    checkedIndexes={[0, 3]}
+                    tasksIndexes={[0, 1, 2, 3]}
                     tasks={bugs}
                   />
                 )
@@ -218,8 +218,8 @@ export default function Dashboard() {
                 tabIcon: Code,
                 tabContent: (
                   <Tasks
-                    checkedIndexes={[ 0 ]}
-                    tasksIndexes={[ 0, 1 ]}
+                    checkedIndexes={[0]}
+                    tasksIndexes={[0, 1]}
                     tasks={website}
                   />
                 )
@@ -229,8 +229,8 @@ export default function Dashboard() {
                 tabIcon: Cloud,
                 tabContent: (
                   <Tasks
-                    checkedIndexes={[ 1 ]}
-                    tasksIndexes={[ 0, 1, 2 ]}
+                    checkedIndexes={[1]}
+                    tasksIndexes={[0, 1, 2]}
                     tasks={server}
                   />
                 )
@@ -249,12 +249,12 @@ export default function Dashboard() {
             <CardBody>
               <Table
                 tableHeaderColor="warning"
-                tableHead={[ "ID", "Name", "Salary", "Country" ]}
+                tableHead={["ID", "Name", "Salary", "Country"]}
                 tableData={[
-                  [ "1", "Dakota Rice", "$36,738", "Niger" ],
-                  [ "2", "Minerva Hooper", "$23,789", "Curaçao" ],
-                  [ "3", "Sage Rodriguez", "$56,142", "Netherlands" ],
-                  [ "4", "Philip Chaney", "$38,735", "Korea, South" ]
+                  ["1", "Dakota Rice", "$36,738", "Niger"],
+                  ["2", "Minerva Hooper", "$23,789", "Curaçao"],
+                  ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
+                  ["4", "Philip Chaney", "$38,735", "Korea, South"]
                 ]}
               />
             </CardBody>
--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -5,8 +5,8 @@ module.exports = {
       port: 8545,
       network_id: "*",
       gas: 30000000,
-      // from: '0xe8543ceDAd017D57cda9EEA51322af431B84D46e',
-      from: '0x91DbC8EF897931d08dE34CBB846e3a357B8D1552'
+      from: '0xe8543ceDAd017D57cda9EEA51322af431B84D46e',
+      // from: '0x91DbC8EF897931d08dE34CBB846e3a357B8D1552'
     },
   },
   contracts_directory: './testing/',
